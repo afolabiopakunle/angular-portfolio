8d4f21a1404ca0164770225e400cd5626916cba7
Afolabi Opakunle <opakunleafolabi@gmail.com>
2021-04-21 15:53:27 +0100

    introJS working

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import * as introJs from 'intro.js';
 import { Card } from './card/models/card';
 
 @Component({
@@ -33,12 +34,58 @@ export class AppComponent {
     body: 'Search enabled shopping list app, constructed with the use of   plain JavaScript with commented jQuery version included.',
     href: '#'
   },
-{
-  title: 'My Designs',
-  body: 'Page contains screenshots of previous designs and concepts I delivered using Figma and AdobeXD in some projects',
-  href: '#'
-}];
-  constructor() {
+  {
+    title: 'My Designs',
+    body: 'Page contains screenshots of previous designs and concepts I delivered using Figma and AdobeXD in some projects',
+    href: '#'
+  }];
 
+  intoJS = introJs();
+
+  startIntro() {
+    this.intoJS.start()
+  }
+
+  
+  constructor() {
+    this.intoJS.setOptions({
+      steps: [{
+        intro: "<div style='text-align: center'><h4>Hi!</h4><p>Let me show around</p></div>",
+      },
+      {
+        element: "#step1",
+        intro: "<div style='text-align: center'>Each card links to a live project!</div>",
+        position: 'top',
+        tooltipClass: 'special'
+      },
+      {
+        element: "#step2",
+        intro: "The name ;)",
+        position: 'bottom'
+      },
+      {
+        element: '#step3',
+        intro: 'Official Accounts',
+        // position: 'left',
+      },
+      {
+        element: '#step4',
+        intro: "Get in touch via email.",
+        position: 'top'
+      },
+      {
+        element: '#step5',
+        intro: '<p style="text-align: center">Thanks!</p>'
+      }
+      ],
+      "showBullets": false,
+      "exitOnOverlayClick": false,
+      "showProgress": true,
+      "overlayOpacity": 0.7
+    })
+  }
+
+  ngOnInit() {
+    this.intoJS.start();
   }
 }
