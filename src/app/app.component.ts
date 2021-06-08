7973ba3aa8833d0edb28e5ed4cf91bef1aeca9c2
Afolabi Opakunle <opakunleafolabi@gmail.com>
2021-06-08 10:19:57 +0100

    searchable angular harry porter characters added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,36 +9,42 @@ import { Card } from './card/models/card';
 })
 export class AppComponent {
   title = 'PortfolioAfolabi';
-  cards: Card[] = [{
-    title: 'Sports Website Project',
-    body: 'Adrenalined filled sports webpage with trendy color selection and user-friendly backend cms functionalities.',
-    href: 'https://www.sportsglobaltv.com/'
-  },
-  {
-    title: 'Complete Errands App | JavaScript & Bootstrap',
-    body: 'Do you need quick services of regular daily people to help do stuffs for you?',
-    href: '#'
-  },
-  {
-    title: 'Playstore App Landing Page',
-    body: 'Landing page design has evolved over a short period of time. This design is modern and ageless.',
-    href: '#'
-  },
-  {
-    title: 'Hotel Booking Website',
-    body: 'This website is targeted at converting visitors to book an apartment with Martinas.',
-    href: 'https://martinasapartments.com/',
-  },
-  {
-    title: 'JavaScript Shopping List',
-    body: 'Search enabled shopping list app, constructed with the use of   plain JavaScript with commented jQuery version included.',
-    href: '#'
-  },
-  {
-    title: 'My Designs',
-    body: 'Page contains screenshots of previous designs and concepts I delivered using Figma and AdobeXD in some projects',
-    href: 'https://dribbble.com/afolabiopakunle'
-  }];
+  cards: Card[] = [
+    {
+      title: 'Angular Harry Porter Searchable Characters',
+      body: 'Angular based searchable Harry Porter series characters list. Populates on key up and backspace',
+      href: 'https://opakunleafolabi-angular-harry-porter-search.surge.sh'
+    }, 
+    {
+      title: 'Angular Events Site',
+      body: 'Keep up to date with recent Angular trends and events around your city. Book, register and get notification.',
+      href: 'https://www.sportsglobaltv.com/'
+    },
+    {
+      title: 'Complete Errands App | JavaScript & Bootstrap',
+      body: 'Do you need quick services of regular daily people to help do stuffs for you?',
+      href: '#'
+    },
+    {
+      title: 'Playstore App Landing Page',
+      body: 'Landing page design has evolved over a short period of time. This design is modern and ageless.',
+      href: '#'
+    },
+    {
+      title: 'Hotel Booking Website',
+      body: 'This website is targeted at converting visitors to book an apartment with Martinas.',
+      href: 'https://martinasapartments.com/',
+    },
+    {
+      title: 'JavaScript Shopping List',
+      body: 'Search enabled shopping list app, constructed with the use of   plain JavaScript with commented jQuery version included.',
+      href: '#'
+    },
+    {
+      title: 'My Designs',
+      body: 'Page contains screenshots of previous designs and concepts I delivered using Figma and AdobeXD in some projects',
+      href: 'https://dribbble.com/afolabiopakunle'
+    }];
 
   intoJS = introJs();
 
